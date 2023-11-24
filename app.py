5d305afc3056a692eb2bb37fc65a780e7cf72a0e
mohammed-alaa40123 <66653422+mohammed-alaa40123@users.noreply.github.com>
2023-11-24 18:28:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import requests
 import streamlit as st
 import os
-API = os.getenv("api_key")
+API = os.getenv("API_KEY")
 API_URL = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
 headers = {"Authorization": f"Bearer {API}"}
 
